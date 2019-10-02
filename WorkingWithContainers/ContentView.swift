16de610b638ebbd9118d9aec2bbeb66996aff633
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-10-02 16:49:44 +0300

    How to embed a view in a navigation view

--- WorkingWithContainers/ContentView.swift
+++ WorkingWithContainers/ContentView.swift
@@ -10,7 +10,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
+        NavigationView {
+            Text("SwiftUI")
+                .navigationBarTitle("Welcome")
+        }.navigationBarTitle("Welcome", displayMode: .inline)
     }
 }
 
